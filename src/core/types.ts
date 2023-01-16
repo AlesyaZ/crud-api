925bc2c8cbd74e744d3e03a64765c6408a6ba42b
alesyaz <alesya.zarembo@mail.ru>
2023-01-16 15:02:23 +0300

    feat: connect the methods and add error handl

--- src/core/types.ts
+++ src/core/types.ts
@@ -5,4 +5,5 @@ export const enum ResStatusCode {
   Bad_Request = 400,
   Not_Found = 404,
   Internal_Server_Error = 500,
+  Not_Implemented = 501,
 }
--- src/server/requestHandler.ts
+++ src/server/requestHandler.ts
@@ -1,20 +1,49 @@
 import { IncomingMessage, ServerResponse } from 'http';
 import { ResStatusCode } from '../core/types';
+import { messagesErr } from '../core/constants';
+import { handlerGetMethod } from './handlerMethods/GetMethod';
+import { handlerPostMethod } from './handlerMethods/PostMethod';
+import { handlerPutMethod } from './handlerMethods/PutMethod';
+import { handlerDeleteMethod } from './handlerMethods/DeleteMethod';
+import { errorResponse } from '../core/response/error';
 
-export const requestHandler = (req: IncomingMessage, res: ServerResponse) => {
+export const requestHandler = async (
+  req: IncomingMessage,
+  res: ServerResponse,
+) => {
   try {
-    switch (req.method) {
+    const { url, method } = req;
+    const userURL = /\/api\/users/.test(url);
+    if (url && !url.startsWith('/api/users') && !userURL) {
+      errorResponse(res, ResStatusCode.Not_Found, messagesErr.Endpoint_Error);
+      return;
+    }
+
+    switch (method) {
       case 'GET':
+        await handlerGetMethod(req, res);
         break;
       case 'POST':
+        await handlerPostMethod(req, res);
         break;
       case 'PUT':
+        await handlerPutMethod(req, res);
         break;
       case 'DELETE':
+        await handlerDeleteMethod(req, res);
         break;
       default:
+        errorResponse(
+          res,
+          ResStatusCode.Bad_Request,
+          messagesErr.Implemented_Error,
+        );
     }
-  } catch {}
-  res.writeHead(200);
-  res.end(`Server OK!`);
+  } catch {
+    errorResponse(
+      res,
+      ResStatusCode.Internal_Server_Error,
+      messagesErr.Server_Error,
+    );
+  }
 };
